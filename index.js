0d0ae59da093f2c21fe372d6fa1008097efe1c46
Shamialrahad <rahad15-7349@diu.edu.bd>
2021-06-28 18:37:48 +0600

    Add deposit and withdraw

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click",function(){
     loginArea = document.getElementById("login-area");
@@ -5,3 +6,34 @@ loginBtn.addEventListener("click",function(){
     transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
 })
+
+//deposit button event handler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click",function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+    
+    // const currentDeposit = document.getElementById("currentDeposit").innerText;
+    // const currentDepositNumber = parseFloat(currentDeposit);
+    // const totalDeposit = depositNumber+currentDepositNumber;
+
+    // document.getElementById("currentDeposit").innerText = totalDeposit;
+
+    // const currentBalance = document.getElementById("currentBalance").innerText;
+    // const currentBalanceNumber = parseFloat(currentBalance);
+    // const totalBalance = currentBalanceNumber + depositNumber;
+    // document.getElementById("currentBalance").innerText = totalBalance;
+
+    updateSpanText("currentDeposit",depositNumber);
+    updateSpanText("currentBalance",depositNumber);
+
+    document.getElementById("depositAmount").value = "";
+
+})                                                  
+function updateSpanText(id,depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = depositNumber + currentNumber;
+    document.getElementById(id).innerText = total;
+}
+
